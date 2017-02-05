fc32563eaf2808d0c978486888f5ec44d47cb2c7
Evgen <putjatingenja@gmail.com>
2017-02-05 16:56:14 +0200

    WeatherWin

---
+++ WeatherWin/DataWeather.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace WeatherWin
+{
+    class DataWeather
+    {
+        WebRequest getDataHTML;
+        string dataHTML;
+        string temperature;
+        string town;
+        string osadki;
+        string wind;
+        string naprav;
+        string davlenie;
+
+        public DataWeather()
+        {
+            getDataHTML = WebRequest.Create(@"https://www.gismeteo.ua/weather-kharkiv-5053/");
+        }
+
+        public string DataHTML()
+        {
+            try
+            {
+                Stream stream = getDataHTML.GetResponse().GetResponseStream();
+                StreamReader sr = new StreamReader(stream);
+                dataHTML = sr.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                
+            }
+                
+            return dataHTML;
+        }
+
+        public string Town()
+        {
+            town = new Regex(@"<h2 class=""typeM"">(?<town>.*)</h2>").Match(dataHTML).Groups["town"].Value;
+
+            return town;
+        }
+
+        public string Temperature()
+        {
+            string pattern = "&minus;";
+            string repattern = "-";
+            temperature = new Regex(@"<dd class='value m_temp c'>(?<temperature>.*)<span class=""meas"">&deg;C</span></dd>").Match(dataHTML).Groups["temperature"].Value;
+            string temp = Regex.Replace(temperature, pattern, repattern);
+            return temp;
+        }
+
+        public string Osadki()
+        {
+            osadki = new Regex(@"<td>(?<osadki>.*)</td>").Match(dataHTML).Groups["osadki"].Value;
+
+            return osadki;
+        }
+        public string Wind()
+        {
+            wind = new Regex(@"<dd class='value m_wind ms' style='display:inline'>(?<wind>.*)<span class=""unit"">м/с</span></dd>").Match(dataHTML).Groups["wind"].Value;
+            return wind;
+        }
+        public string Davlenie()
+        {
+            davlenie = new Regex(@"<dd class='value m_press torr'>(?<davlenie>.*)<span class=""unit"">мм рт. ст.</span></dd>").Match(dataHTML).Groups["davlenie"].Value;
+            return davlenie;
+        }
+        public string Napravlenie()
+        {
+            naprav = new Regex(@"<dt>(?<naprav>.*)</dt>").Match(dataHTML).Groups["naprav"].Value;
+            return naprav;
+        }
+    }
+}
---
+++ WeatherWin/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WeatherWin
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataWeather DW = new DataWeather();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DW.DataHTML();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Town.Text = DW.Town();
+            Temperature.Text = DW.Temperature();
+            osadki.Text = DW.Osadki();
+            speed.Text = DW.Wind();
+            Napravlenie.Text = DW.Napravlenie();
+            davlenie.Text = DW.Davlenie();
+        }
+    }
+}
